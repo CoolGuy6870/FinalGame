f175fd2f0daba7d078a3607b21df60b2bc25eb5d
CoolGuy6870 <aadisharma0226@gmail.com>
2018-07-02 12:52:48 -0700

    image

--- core/src/main/java/com/missionbit/game/FinalGame.java
+++ core/src/main/java/com/missionbit/game/FinalGame.java
@@ -1,7 +1,72 @@
 package com.missionbit.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.Random;
+
 public class FinalGame extends ApplicationAdapter {
+    private OrthographicCamera camera;
+    private Random randomSource;
+    private Sprite myImage;
+    private SpriteBatch myBatch;
+    private Vector2 velocity;
+
+
+    @Override
+    public void create() {
+        myImage = new Sprite( new Texture(Gdx.files.internal("player.jpg")));
+        myImage.setX(0);
+        myImage.setY(200);
+        randomSource = new Random();
+
+        // Set up camera for 2d view of 800x480 pixels
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        // Create a sprite batch for rendering our image
+        myBatch = new SpriteBatch();
+
+        //TODO: Load our image
+        // Create a random X and Y velocity
+        velocity = new Vector2( 0,  100);
+    }
+
+    @Override
+    public void render() {
+
+        // Clear the screen
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Set up our camera
+        camera.update();
+        myBatch.setProjectionMatrix(camera.combined);
+
+        //TODO: Draw our image!
+        float xPos = myImage.getX() + velocity.x * Gdx.graphics.getDeltaTime();
+        float yPos = myImage.getY() + velocity.y * Gdx.graphics.getDeltaTime();
+
+        myImage.setX(xPos);
+        myImage.setY(yPos);
+
+        myBatch.begin();
+        myImage.draw(myBatch);
+        myBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        myBatch.dispose();
+    }
 }
