0f63f0b29a2098cb02469bc5561067e069b03107
CoolGuy6870 <aadisharma0226@gmail.com>
2018-07-05 10:48:25 -0700

    Boundaries for obsticles

--- core/src/main/java/com/missionbit/game/Obstacles.java
+++ core/src/main/java/com/missionbit/game/Obstacles.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.math.Vector3;
 public class Obstacles {
 
     private Sprite myImage;
+    private Sprite myImage2;
     private Vector2 velocity;
     private SpriteBatch myBatch;
     private ParticleEffect effect;
@@ -22,11 +23,13 @@ public class Obstacles {
     public Obstacles(SpriteBatch batch) {
         myBatch = batch;
         myImage = new Sprite(new Texture(Gdx.files.internal("tree.png")));
+        myImage2 = new Sprite(new Texture(Gdx.files.internal("rock.png")));
+
 
 //        effect = new ParticleEffect();
 //        effect.load(Gdx.files.internal("effects/explode.p"), Gdx.files.internal("images"));
 
-        velocity = new Vector2(MathUtils.random() * 300, MathUtils.random() * 300);
+        velocity = new Vector2(MathUtils.random() * -300, MathUtils.random() * 300);
     }
 
     public void update() {
@@ -35,14 +38,17 @@ public class Obstacles {
         float xPos = myImage.getX() + velocity.x * Gdx.graphics.getDeltaTime();
         float yPos = myImage.getY() + velocity.y * Gdx.graphics.getDeltaTime();
 
+
         myImage.setX(xPos);
         myImage.setY(yPos);
+        myImage2.setX(xPos);
+        myImage2.setY(yPos);
 
         Gdx.graphics.getWidth();
         Gdx.graphics.getHeight();
 
-        if (myImage.getX() < 0) {
-            myImage.setX(0);
+        if (myImage.getX() < 230) {
+            myImage.setX(230);
             velocity.x *= -1;
         }
 
@@ -51,8 +57,11 @@ public class Obstacles {
             velocity.y *= -1;
         }
 
-        if (myImage.getX() + myImage.getWidth() > Gdx.graphics.getWidth()) {
-            myImage.setX(Gdx.graphics.getWidth() - myImage.getWidth());
+
+
+
+        if (myImage.getX() > 660) {
+            myImage.setX(660);
             velocity.x *= -1;
         }
 
